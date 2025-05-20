481799bf51d3daacf1a238a3c65aba145227b286
navadeep-17 <navadeepthota17@gmail.com>
2025-05-20 17:44:43 +0530

    resolved some backend issue

--- Backend/routes/commentRoutes.js
+++ Backend/routes/commentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Comment = require('../models/comment');
+const Comment = require('../models/Comment');
 const { authMiddleware } = require('../middleware/authMiddleware');
 
 // POST: Add a comment to an experience
